9321ac8687f99060e4a5ac4f0a95de56739aa36a
Kuch Darith <kuchdarith@KuchdeMacBook-Pro.local>
2024-04-02 21:39:21 +0700

    add

--- lib/src/flutter_pull_up_down_refresh_base.dart
+++ lib/src/flutter_pull_up_down_refresh_base.dart
@@ -1,17 +1,45 @@
 // TODO: Put public facing types in this file.
 import 'package:flutter/material.dart';
 
-/// Checks if you are awesome. Spoiler: you are.
+/// FlutterPullUpDownRefresh is the main class of the FlutterPullUpDownRefresh API.
+///
+/// It manages all params required for API using.
+/// you can check some required, some not.
 class FlutterPullUpDownRefresh extends StatelessWidget {
+  /// The [child] from user input.
+  ///
+  /// Ex: Text(""), ListView.Build().
   final Widget child;
+
+  /// The [isLoading] show progress loading view.
+  ///
+  /// The value true, false
   final bool isLoading;
+
+  /// The [showRefreshIndicator] show indicator pull refresh.
+  ///
+  /// The value true, false
   final bool showRefreshIndicator;
+
+  /// The [loadingColor] change loading colors.
   final Color? loadingColor;
+
+  /// The [loadingBgColor] change backkground loading colors.
   final Color? loadingBgColor;
+
+  /// The [refreshIndicatorColor] change indicator pull refresh  colors.
   final Color? refreshIndicatorColor;
+
+  /// The [onRefresh] this for listen onRefresh function.
   final Function? onRefresh;
+
+  /// The [scrollController] scrollcontroller class.
   final ScrollController? scrollController;
+
+  /// The [scrollController] this for listen onAtBottom function.
   final Function(bool)? onAtBottom;
+
+  /// The [scrollController] this for listen onAtTop function.
   final Function(bool)? onAtTop;
 
   const FlutterPullUpDownRefresh({
